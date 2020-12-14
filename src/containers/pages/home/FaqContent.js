5760d068f07f243ece5c6c88b1be9b1eb4e7bd32
pureworldnew <pureworldnew@gmail.com>
2020-12-14 16:14:21 +0800

    complete the QAQ part

--- src/containers/pages/home/FaqContent.js
+++ src/containers/pages/home/FaqContent.js
@@ -1,7 +1,319 @@
 import React, { useState } from 'react';
 
 const FaqContent = ({}) => {
-  return <section className="dirt-section">Faq Content</section>;
+  return (
+    <section className="faq-content-section mt-40 pb-80">
+      <div className="container">
+        <div className="row">
+          <div className="row">
+            <div class="col-sm-12">
+              <div class="dirt_heading">
+                <h2>FAQs</h2>
+              </div>
+
+              <div>
+                <h2 class="custom_headein_h2">
+                  How do I advertise on this web site?
+                </h2>
+                <p>
+                  By using our 'contact us' form, feel free to ask any question
+                  you may have. Our personal relationship with you is important,
+                  so we will reply as soon as possible.
+                </p>
+                <p>&nbsp;</p>
+              </div>
+              <div>
+                <h2 class="custom_headein_h2">
+                  Who benefits from the use of this web site?
+                </h2>
+                <p>
+                  Members benefit by getting fill, which is free most of the
+                  time. People in need to get rid of fill benefit by finding
+                  close dump sites. The environment benefits with less machinery
+                  use and travel etc.
+                </p>
+                <p>&nbsp;</p>
+              </div>
+              <div>
+                <h2 class="custom_headein_h2">
+                  Can I list more than one site at a time?
+                </h2>
+                <p>There is no limit on the number of listings you may have.</p>
+                <p>&nbsp;</p>
+              </div>
+              <div>
+                <h2 class="custom_headein_h2">
+                  What is this web site all about?
+                </h2>
+                <p>
+                  This web site is designed to link people and companies
+                  together who either want fill / soil or need soil / fill. It's
+                  a great interface and is very easy to use. Also, other
+                  materials can be listed if desired.
+                </p>
+                <p>&nbsp;</p>
+              </div>
+              <div>
+                <h2 class="custom_headein_h2">Why sign up ?</h2>
+                <p>
+                  Why sign up ? its FREE! why wouldn't you? Get access to all
+                  'need' and 'have fill' listings, post all the listings you
+                  want, find contractors in your area! Have a company? why not
+                  add it to our database, so our visitors can contact you or
+                  vise versa. How does it work ? Lets say you need dirt, all you
+                  have to do is register, post 'need' or 'have fill' and wait
+                  for someone to contact you. Its that easy!
+                </p>
+                <p>&nbsp;</p>
+              </div>
+              <div>
+                <h2 class="custom_headein_h2">
+                  {' '}
+                  What is the standard units of measure?
+                </h2>
+                <p>
+                  Cubic Yards or Tons in standard size loads. How many
+                  wheelbarrow loads are there in a cubic yard? Usually it will
+                  take 9 loads in a 3 cubic foot wheelbarrow to carry one cubic
+                  yard. How much area does a cubic yard cover? At a depth of 3
+                  inches, one cubic yard covers approximately 100 s.f. two
+                  inches approximately 200 s.f., or one inch at 300 s.f. Note:
+                  Coverage is an estimate and varies based on job conditions.
+                </p>
+                <p>&nbsp;</p>
+              </div>
+              <div>
+                <h2 class="custom_headein_h2">What is clean fill dirt?</h2>
+                <p>
+                  Thats really the million-dollar question, isnt it? Clean fill
+                  dirt is a dirt fill that is easy to explain, but somewhat odd
+                  to comprehend. In short, this dirt fill is literally
+                  artificial dirt. It has the same characteristics of dirt in
+                  that it can be used to stabilize a foundation, fill in hole
+                  left by your weekend projects, and act as dirt in the event
+                  that you do not have enough dirt to finish a project
+                  completely. In short, while it is not dirt (and usually does
+                  not share any elemental characteristics with dirt), clean fill
+                  dirt behaves exactly like dirt.
+                </p>
+                <p>&nbsp;</p>
+              </div>
+              <div>
+                <h2 class="custom_headein_h2">
+                  What is clean fill dirt (and why homeowners should care)
+                </h2>
+                <p>
+                  If you have ever seen a sign on someones lawn that says, Clean
+                  fill dirt wanted, or clean fill dirt available, then you have
+                  likely been confused as to what this exactly means. Its
+                  understandable, as the term does not actually tell you
+                  anything about what it actually is. However, clean fill dirt
+                  is an important product for individuals of all walks of life,
+                  especially those who want to perform landscaping construction
+                  on their homes lawn. To understand what clean fill dirt is
+                  (and the significance of it), you need to know what the term
+                  means. To start, here is what the word clean relates to in the
+                  term: &lt;img style="float: left; margin: 5px;"
+                  src="https://cleanfill.net/images/img3.jpg" alt="" width="350"
+                  /&gt; The word clean in the clean fill dirt simply relates to
+                  the dirt fill being environmentally friendly. It is free from
+                  contamination, meaning one will not find anything in the fill
+                  dirt that is corrosive, combustible, radioactive, and so on.
+                  To put it simply, you will never find anything in clean fill
+                  dirt that is harmful to the environment (and if it is, then it
+                  is not truly clean fill dirt). The second part, fill dirt may
+                  seem self-explanatory, but it really is not. Sure, fill dirt
+                  that you find could actually be the type of dirt we are used
+                  to (e.g. topsoil), but there are so many different types of
+                  fill dirt other than this. Clay, sand, gravel, and even brick
+                  or concrete are just some of the types of clean fill dirt that
+                  homeowners typically look for to use for their landscaping
+                  projects. With this being said, we can deduct that clean fill
+                  dirt simply means, environmentally-friendly dirt fill that is
+                  used as dirt. By and large, this is exactly what clean fill
+                  dirt is, and it is the easiest way to apply landscaping to
+                  your home. So lets look at that for a moment: why would
+                  someone want to use this to landscape their homes lawn? Not
+                  only are there a multitude of substances that can be used as
+                  clean fill dirt, there is also a ton of it readily available
+                  for you and many times, for free. Why would someone give away
+                  their clean fill dirt if it is so useful, you ask? Its simple
+                  really: They no longer need it. Perhaps there was clean fill
+                  dirt left over at a construction site, and now they have no
+                  place to take it. Or perhaps someone obtained too much of it
+                  at their own home, and now they no longer need it. Remember
+                  the signs mentioned in the first of the article? This is
+                  exactly why you see those signs. People either have too much
+                  clean fill dirt, or are needing some, and one can be assured
+                  that there is someone who has an excessive amount and will
+                  drop it off for the individual needing the clean fill dirt in
+                  a moments notice. Clean fill dirt is one of the most useful
+                  substances used for landscaping. Acting as the actual earth,
+                  this imitation dirt is absolutely incredible and extremely
+                  versatile, not to mention very useful. The next time you want
+                  to landscape your home, display a sign stating, clean fill
+                  dirt wanted: not only will you receive your free dirt soon,
+                  you will also be saving yourself a lot of time and money, all
+                  the while helping the environment by placing substances that
+                  are non-harmful to the environment into the ground!
+                </p>
+                <p>&nbsp;</p>
+              </div>
+              <div>
+                <h2 class="custom_headein_h2">
+                  What cant clean fill be used for?
+                </h2>
+                <p>
+                  Clean fill dirt is useful for all types of situations, no
+                  matter if you are building onto your home or are building a
+                  new office building on your construction site. Clean fill dirt
+                  ensures that your project never falls behind simply because
+                  you do not have enough dirt to work with. And, because it is
+                  environmentally friendly and easy to come by, clean fill dirt
+                  will ensure that you will assuredly finish your project the
+                  right way the very first time. Just think, you have the
+                  ability to use artificial dirt that is safe for the
+                  environment as well. Take solace in knowing that you are not
+                  polluting the environment, all the while knowing that you may
+                  potentially finish your project ahead of schedule.
+                </p>
+                <p>&nbsp;</p>
+              </div>
+              <div>
+                <h2 class="custom_headein_h2">
+                  Why would someone want to give clean fill away?
+                </h2>
+                <p>
+                  Knowing how useful clean fill dirt can be, this may be a
+                  question on your mind, but it is one that is easily answered.
+                  To put it simply, when someone (or as the case may be,
+                  company) needs to rid of their clean fill dirt, they usually
+                  have two choices: Find someone to take it off their hands.
+                  Throw it away. The last choice is not ideal in any way, as
+                  most people do not want to have to pay any types of fees to
+                  dispose of it properly. Instead, if they can find someone that
+                  needs clean fill dirt for any type of project (or another
+                  company that needs it, say for their construction site), then
+                  they will gladly provide them with free dirt to do with it
+                  however they see fit. Final thoughts on the clean fill dirt
+                  bartering system As you can see, the clean fill dirt bartering
+                  system is very unique. By stating that you want ones leftover
+                  clean fill dirt for free, many individuals are glad to help
+                  you out by providing you with this type of dirt, which in turn
+                  helps them by saving them the hassle of disposing of the dirt.
+                  Its a win-win for everyone, and its one of the many ways clean
+                  fill dirt helps ordinary individuals and companies alike in
+                  getting their jobs done.
+                </p>
+                <p>&nbsp;</p>
+              </div>
+              <div>
+                <h2 class="custom_headein_h2">
+                  The barter system of clean fill dirt?
+                </h2>
+                <p>
+                  Theres an interesting (yet unofficial) bartering system in
+                  place all across North America that you have probably never
+                  heard about. It involves the trading of free dirt, and while
+                  this may seem as if it is a worthless and pointless system,
+                  once you know more about it, it suddenly becomes perfectly
+                  clear: the clean fill dirt bartering system is one of the most
+                  useful bartering systems still in existence. First, heres the
+                  question that is obviously on your mind: why would anyone want
+                  to do this? Its simple really: clean fill dirt is a unique
+                  dirt fill that is both heavily desired and heavily undesired,
+                  and the individuals that desired it the first time are usually
+                  the ones that want to get rid of it as quickly as possible.
+                  Confused? Thats understandable, but here is what we mean.
+                  &lt;img style="float: right; margin: 5px;"
+                  src="https://cleanfill.net/images/img7.jpg" alt="" width="350"
+                  /&gt; Lets start with why someone would want clean fill dirt.
+                  When someone wants to landscape their lawn for a project
+                  around their home (e.g. building an underground swimming pool
+                  could be one of them), then they will likely need excess dirt
+                  to work with. Unfortunately, they probably do not have enough
+                  in their own lawn; therefore, they often look for artificial
+                  dirt, (i.e. the clean fill dirt) to accomplish their project.
+                  The advantage of using clean fill dirt is two-fold: Clean fill
+                  dirt is usually easy to come by, especially since many
+                  individuals who use clean fill dirt usually have a lot left
+                  over. Because of this, many people simply want to get rid of
+                  their clean fill dirt as quickly as possible, and will many
+                  times even give it to someone free of charge. It is
+                  environmentally friendly. The last advantage is a big one:
+                  environmentally friendly. Because you are going to be placing
+                  the dirt fill into the ground, it goes without saying that it
+                  must be friendly to the environment. This will ensure that the
+                  soil around ones lawn remains in a healthy state and
+                  unpolluted. This is important, especially when there are many
+                  homes around the area that want to keep their property value
+                  at a consistent level (can you imagine how bad ones property
+                  value would be with poisoned soil? It would be terrible.).{' '}
+                </p>
+                <p>&nbsp;</p>
+              </div>
+              <div>
+                <h2 class="custom_headein_h2">
+                  What makes clean fill dirt so great?
+                </h2>
+                <p>
+                  What makes this wonder dirt so great is that it is easy
+                  accessible to nearly everyone. It is not hard to find due to
+                  the fact that it can be created from a variety of different
+                  substances (e.g. clay, topsoil, brick, concrete, and so on).
+                  And the best part? It is not harmful to the environment in any
+                  way. Thats right: you can rest easy at night knowing that the
+                  artificial dirt you placed into the ground via your earth
+                  mover will not pollute the soil around the vicinity of your
+                  site in any way. And if you have neighbors, then this is good
+                  news as well as you will not cause them any headache in the
+                  least.{' '}
+                </p>
+                <p>&nbsp;</p>
+              </div>
+              <div>
+                <h2 class="custom_headein_h2">
+                  Using earth movers and clean fill dirt
+                </h2>
+                <p>
+                  It goes without saying that if you are going to be moving the
+                  earth, then you need an abundance of dirt to do it. While you
+                  may think, what you dig up can be placed back into the ground
+                  without any problems or hiccups whatsoever, this train of
+                  thought is very wrong. Rather, you need some sort of dirt fill
+                  that you can use in place of dirt, so you can fill your holes,
+                  reshape the landscape, and generally get your project
+                  accomplished and fully completed on time without having to
+                  worry about gathering more dirt. How do you do this? Its easy:
+                  through clean fill dirt.
+                </p>
+                <p>&nbsp;</p>
+              </div>
+              <div>
+                <h2 class="custom_headein_h2">
+                  How does it relate to earth movers?
+                </h2>
+                <p>
+                  This is a question you are likely asking yourself, however it
+                  is easily answerable. If earth moving must be done for any
+                  reason, then you owe it to yourself to obtain clean fill dirt
+                  right away. You may not even need it, but chances are high
+                  that you will (and even if you dont, it is easy to give away
+                  as many individuals and construction companies are always
+                  looking for clean fill dirt to finish their projects or
+                  construction sites). This will save you the trouble of having
+                  to look for extra dirt in the long run, and you can be assured
+                  that you will never fall behind due to the lack of dirt when
+                  you have an abundance of clean fill dirt at your job site.
+                </p>
+                <p>&nbsp;</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default FaqContent;
